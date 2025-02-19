1c98e2f4f335158303773d0e95a9d3a331c238cf
Corbio <113342618+Corbiobio@users.noreply.github.com>
2025-02-19 17:29:40 +0100

    add julia and mandelbrot func, rename some func

--- src/fractol.c
+++ src/fractol.c
@@ -6,7 +6,7 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 12:27:31 by edarnand          #+#    #+#             */
-/*   Updated: 2025/02/18 16:07:36 by edarnand         ###   ########.fr       */
+/*   Updated: 2025/02/19 17:22:43 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,54 +21,94 @@ void	draw_pixel(t_img *img, int x, int y, unsigned int color)
 {
 	char	*pt;
 
-	pt = img->addr + (img->line_length * y + x * (img->bits_per_pixel / 8));
+	pt = img->addr + (img->line_length * y + x * img->bits_per_pixel);
 	*(unsigned int*)pt = color;
 }
 
-void	draw_fractal(t_img *img, t_complex *comp, int screen_width)
+int	julia(t_complex *comp, double c_real, double c_im, int max_iteration)
 {
-	int	y = 0;
-	double c_im = comp->im_start;
+	double	tmp_real;
+	int		index;
+	double real;
+	double im;
+
+	real = comp->real_curr;
+	im = comp->im_curr;
+	index = 0;
+	while (index < max_iteration)
+	{
+		//nb complex to square = r^2 - im^2 + 2*r*im
+		//formula mandelbrot = complex^2 + c
+		tmp_real = real * real - im * im;
+		im = 2 * real * im + c_im;
+		real = tmp_real + c_real;
+		if (real > 2.1 || im > 2.1)
+			return (index);
+		index++;
+	}
+	return (index);
+}
+
+int	mandelbrot(double c_real, double c_im, int max_iteration)
+{
+	double	real;
+	double	im;
+	double	tmp_real;
+	int		index;
+
+	real = 0;
+	im = 0;
+	index = 0;
+	while (index < max_iteration)
+	{
+		//nb complex to square = r^2 - im^2 + 2*r*im
+		//formula mandelbrot = complex^2 + c
+		tmp_real = real * real - im * im;
+		im = 2 * real * im + c_im;
+		real = tmp_real + c_real;
+		if (real > 2.1 || im > 2.1)
+			return (index);
+		index++;
+	}
+	return (index);
+}
+
+void	draw_fractal(t_img *img, t_complex *comp, int screen_width, int max_iteration)
+{
+	int index;
+	int	x;
+	int	y;
+
+	y = 0;
+	comp->im_curr = comp->im_start;
 	while (y < SCREEN_HEIGHT)
 	{
-		int	x = 0;
-		double c_real = comp->real_start;
+		x = 0;
+		comp->real_curr = comp->real_start;
 		while (x < screen_width)
 		{
-			double r = 0;
-			double im = 0;
-			int i = 0;
-			while (i < 51)
-			{
-				//nb complex to square = r^2 - im^2 + 2*r*im
-				//formula mandelbrot = complex^2 + c
-				double tmp_real = r * r - im * im;
-				im = 2 * r * im + c_im;
-				r = tmp_real + c_real;
-				if (r > 2 || im > 2)
-					break;
-				i++;
-			}
-			if (i == 51)
+			index = julia(comp, -1.76733, 0.00002, max_iteration);
+			//index = mandelbrot(comp->real_curr, comp->im_curr, max_iteration);
+			if (index == max_iteration)
 				draw_pixel(img, x, y, create_rgb(0, 0, 0));
 			else
-				draw_pixel(img, x, y, create_rgb(100, 0, i * 5));
-			c_real += comp->real_range_per_px;
+				draw_pixel(img, x, y, create_rgb(100, 0, index*5));
+			comp->real_curr += comp->real_range_per_px;
 			x++;
 		}
-		c_im += comp->im_range_per_px;
+		comp->im_curr += comp->im_range_per_px;
 		y++;
 	}
 }
 
-void	handle_offset(t_complex *comp, int key)
+void	move_fractal(t_complex *comp, int key)
 {
-	const double	real_offset = comp->real_range / 100;
-	const double	im_offset = comp->im_range / 100;
+	const double	real_offset = comp->real_range / 50;
+	const double	im_offset = comp->im_range / 50;
 
 	if (key == 'w')
 	{
-		comp->im_start -= im_offset;
+	comp->im_start -= im_offset;
 		comp->im_end -= im_offset;
 	}
 	else if (key == 's')
@@ -88,10 +128,10 @@ void	handle_offset(t_complex *comp, int key)
 	}
 }
 
-void	handle_key_zoom(t_complex *comp, int screen_width, int key)
+void	key_zoom(t_complex *comp, int screen_width, int key)
 {
-	const double	real_offset = comp->real_range / 100;
-	const double	im_offset = comp->im_range / 100;
+	const double	real_offset = comp->real_range / 40;
+	const double	im_offset = comp->im_range / 40;
 
 	if (key == '=')
 	{
@@ -114,14 +154,14 @@ int	handle_all_key_input(int key, t_data *data)
 {
 	if (key == 'w' || key == 'a' || key == 's' || key == 'd')
 	{
-		handle_offset(data->comp, key);
-		draw_fractal(data->img, data->comp, data->screen_width);
+		move_fractal(data->comp, key);
+		draw_fractal(data->img, data->comp, data->screen_width, data->max_iteration);
 		mlx_put_image_to_window(data->mlx, data->mlx_wind, data->img->img, 0, 0);
 	}
 	else if (key == '-' || key == '=')
 	{
-		handle_key_zoom(data->comp, data->screen_width, key);
-		draw_fractal(data->img, data->comp, data->screen_width);
+		key_zoom(data->comp, data->screen_width, key);
+		draw_fractal(data->img, data->comp, data->screen_width, data->max_iteration);
 		mlx_put_image_to_window(data->mlx, data->mlx_wind, data->img->img, 0, 0);
 	}
 	return (0);
@@ -154,7 +194,7 @@ int	handle_all_mouse_input(int key, int x, int y, t_data *data)
 	if (key == 4 || key == 5)
 	{
 		handle_mouse_zoom(data->comp, data->screen_width, key, x, y);
-		draw_fractal(data->img, data->comp, data->screen_width);
+		draw_fractal(data->img, data->comp, data->screen_width, data->max_iteration);
 		mlx_put_image_to_window(data->mlx, data->mlx_wind, data->img->img, 0, 0);
 	}
 	return (0);
@@ -167,11 +207,12 @@ int	main(int ac, char **av)
 	data = init_data();
 	if (data == NULL)
 		printf("error");//FIX exit ?
-	mlx_key_hook(data->mlx_wind, &handle_all_key_input, data);
+	mlx_hook(data->mlx_wind, KeyPress, KeyPressMask ,&handle_all_key_input, data);
 	mlx_mouse_hook(data->mlx_wind, &handle_all_mouse_input, data);
-	draw_fractal(data->img, data->comp, data->screen_width);
+	draw_fractal(data->img, data->comp, data->screen_width, data->max_iteration);
 	mlx_put_image_to_window(data->mlx, data->mlx_wind, data->img->img, 0, 0);
 	mlx_loop(data->mlx);
+	//mlx_key_hook(data->mlx_wind, &handle_all_key_input, data);
 	//mlx_destroy_image(data->mlx, data->img->img);
 	//mlx_destroy_window(data->mlx, data->mlx_wind);
 	//mlx_destroy_display(data->mlx);
