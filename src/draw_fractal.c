6a7d3f2a445067819e3e2ebd744d17d634e0a4bc
Corbio <113342618+Corbiobio@users.noreply.github.com>
2025-06-05 17:59:04 +0200

    improve how key are handle, add sceenshot

--- src/draw_fractal.c
+++ src/draw_fractal.c
@@ -6,21 +6,50 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 10:35:38 by edarnand          #+#    #+#             */
-/*   Updated: 2025/05/09 17:47:05 by edarnand         ###   ########.fr       */
+/*   Updated: 2025/06/05 17:56:43 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+#include "color.h"
 #include "mlx.h"
+#include <fcntl.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+void write_bmp_header(int fd, int width, int height)
+{
+    unsigned char header[54] = {0};
+    int filesize = 54 + 4 * width * height;
+
+    header[0] = 'B';
+    header[1] = 'M';
+    *(int*)&header[2] = filesize;
+    header[10] = 54;
+    header[14] = 40;
+    *(int*)&header[18] = width;
+    *(int*)&header[22] = -height;
+    header[26] = 1;
+    header[28] = 32;
+    write(fd, header, 54);
+}
 
 void	draw_fractal(t_data *data)
 {
 	calcul_fractal(data);
 	mlx_put_image_to_window(data->mlx, data->mlx_wind,
 		data->img->img, 0, 0);
+	int fd = open("screen.bmp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (fd < 0)
+    {
+        write(2, "Failed to open file\n", 20);
+        return ;
+    }
+    write_bmp_header(fd, SCREEN_WIDTH, SCREEN_HEIGHT);
+    write(fd, data->img->addr, SCREEN_HEIGHT * data->img->line_length);
+    close(fd);
 }
 
 void	draw_pixel(t_img *img, int x, int y, unsigned int color)
@@ -48,9 +77,10 @@ void	*calc_line(void *line_data)
 		while (x < SCREEN_WIDTH)
 		{
 			iteration = line->fractal_func(&line->comp, max_iteration);
-			if (y == line->y_stop - 1)
-				draw_pixel(line->img, x, y, 0x11111);
-			else if (iteration == max_iteration)
+			//if (y == line->y_stop - 1)
+			//	draw_pixel(line->img, x, y, 0x11111);
+			//else
+			if (iteration == max_iteration)
 				draw_pixel(line->img, x, y, 0x000000);
 			else
 				draw_pixel(line->img, x, y, get_color_form_palet(iteration,
@@ -64,26 +94,48 @@ void	*calc_line(void *line_data)
 	return (NULL);
 }
 
-#define CPU 5
+#define CPU 8
 
 void	calcul_fractal(t_data *data)
 {
 	const int	y_per_cpu = SCREEN_HEIGHT / CPU;
 	t_line_data	line;
+	t_line_data	lines[CPU];
+	pthread_t	thrs[CPU];
 	int			i;
 
 	i = 0;
 	data->comp->im_curr = data->comp->im_start;
+
 	line.img = data->img;
 	line.fractal_func = data->fractal_func;
 	line.data = data;
 	line.comp = *data->comp;
+	line.y_start = i * y_per_cpu;
+	line.y_stop = i * y_per_cpu + y_per_cpu;
+
 	while (i < CPU)
 	{
+		lines[i].img = data->img;
+		lines[i].fractal_func = data->fractal_func;
+		lines[i].data = data;
+		lines[i].comp = *data->comp;
+		lines[i].y_start = i * y_per_cpu;
+		lines[i].comp.im_curr += y_per_cpu * lines[i].comp.im_range_per_px * i;
+		lines[i].y_stop = i * y_per_cpu + y_per_cpu;
+		i++;
+	}
 
-		line.y_start = i * y_per_cpu;
-		line.y_stop = i * y_per_cpu + y_per_cpu;
-		calc_line(&line);
+	i = 0;
+	while (i < CPU)
+	{
+		pthread_create(thrs + i, NULL, &calc_line, lines + i);
+		i++;
+	}
+	i = 0;
+	while (i < CPU)
+	{
+		pthread_join(thrs[i], NULL);
 		i++;
 	}
 }
--- src/key_input.c
+++ src/key_input.c
@@ -6,34 +6,39 @@
 /*   By: edarnand <edarnand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 18:06:53 by edarnand          #+#    #+#             */
-/*   Updated: 2025/05/09 16:03:01 by edarnand         ###   ########.fr       */
+/*   Updated: 2025/06/05 17:50:50 by edarnand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "color.h"
 #include "fractol.h"
 
 static void	move_fractal(t_complex *comp, int key)
 {
-	const double	real_offset = comp->real_range / 50;
-	const double	im_offset = comp->im_range / 50;
+	double	real_offset;
+	double	im_offset;
 
-	if (key == 'w' || key == KEY_UP)
+	if (key == KEY_UP || key == KEY_ARROW_UP)
 	{
+		im_offset = comp->im_range / 50;
 		comp->im_start -= im_offset;
 		comp->im_end -= im_offset;
 	}
-	else if (key == 's' || key == KEY_DOWN)
+	else if (key == KEY_DOWN || key == KEY_ARROW_DOWN)
 	{
+		im_offset = comp->im_range / 50;
 		comp->im_start += im_offset;
 		comp->im_end += im_offset;
 	}
-	else if (key == 'a' || key == KEY_LEFT)
+	else if (key == KEY_LEFT || key == KEY_ARROW_LEFT)
 	{
+		real_offset = comp->real_range / 50;
 		comp->real_start -= real_offset;
 		comp->real_end -= real_offset;
 	}
-	else if (key == 'd' || key == KEY_RIGHT)
+	else if (key == KEY_RIGHT || key == KEY_ARROW_RIGHT)
 	{
+		real_offset = comp->real_range / 50;
 		comp->real_start += real_offset;
 		comp->real_end += real_offset;
 	}
@@ -44,27 +49,42 @@ static void	handle_iteration(int key, t_data *data)
 	if (key == KEY_ADD_ITER)
 		data->max_iteration += 10;
 	else if (key == KEY_REMOVE_ITER)
+	{
 		data->max_iteration -= 10;
+		if (data->max_iteration >= 5)
+			data->max_iteration = 5;
+	}
 	draw_fractal(data);
 }
 
-static void	handle_gradient(t_data *data)
+static void	toggle_gradient(t_data *data)
 {
 	data->has_smooth_gradient = !data->has_smooth_gradient;
 	data->fractal_func = get_fractal_func(data->fractal_id, data);
 	draw_fractal(data);
 }
 
-static void	handle_index_gradient(t_data *data)
+static void	change_colorset_id(t_data *data, int key)
 {
-	data->color_func_id = (data->color_func_id + 1) % 9;
+	int *id;
+
+	id = &data->color_func_id;
+	if (key == KEY_INCREASE_COLORSET)
+		*id = (*id + 1) % COLOR_AMOUNT;
+	else if (key == KEY_DECREASE_COLORSET)
+	{
+		if (*id <= 0)
+			*id = COLOR_AMOUNT -1;
+		else
+			*id = (*id - 1) % COLOR_AMOUNT;
+	}
 	draw_fractal(data);
 }
 
 int	handle_all_key_input(int key, t_data *data)
 {
-	if (key == 'w' || key == 'a' || key == 's' || key == 'd'
-		|| (key >= KEY_LEFT && key <= KEY_DOWN))
+	if (key == KEY_UP || key == KEY_DOWN || key == KEY_LEFT || key == KEY_RIGHT
+		|| (key >= KEY_ARROW_LEFT && key <= KEY_ARROW_DOWN))
 	{
 		move_fractal(data->comp, key);
 		draw_fractal(data);
@@ -74,15 +94,12 @@ int	handle_all_key_input(int key, t_data *data)
 		handle_zoom(data, key, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 		draw_fractal(data);
 	}
-	else if (key == KEY_ADD_ITER
-		|| (key == KEY_REMOVE_ITER && data->max_iteration >= 20))
+	else if (key == KEY_ADD_ITER || key == KEY_REMOVE_ITER)
 		handle_iteration(key, data);
-	else if (key == KEY_GRADIENT)
-		handle_gradient(data);
-	else if (key == KEY_GRADIENT)
-		handle_gradient(data);
-	else if (key == KEY_CHANGE_GRADIENT)
-		handle_index_gradient(data);
+	else if (key == KEY_TOGGLE_GRADIENT)
+		toggle_gradient(data);
+	else if (key == KEY_INCREASE_COLORSET || key == KEY_DECREASE_COLORSET)
+		change_colorset_id(data, key);
 	else if (key == KEY_ESC)
 		exit_close_free_mlx_and_data(data);
 	return (0);
