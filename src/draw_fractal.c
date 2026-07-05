ad1c5610a728f99fa45e917b19a95ca3bb7048bb
Corbiobio <113342618+Corbiobio@users.noreply.github.com>
2026-07-05 19:06:51 +0200

    add colored buddhabrot, but at what cost....

--- src/draw_fractal.c
+++ src/draw_fractal.c
@@ -71,9 +71,10 @@ int	is_in_mandelbrot(double c_real, double c_im, int max_iteration)
 void	budha_calc(t_data *data)
 {
 	srand(1);
-	int arr[SCREEN_WIDTH][SCREEN_HEIGHT];
+	int arr[SCREEN_WIDTH][SCREEN_HEIGHT][3];
 	bzero(arr, sizeof(arr));
 
+	data->max_iteration = 5000;
 	for (int i = 0; i < (SCREEN_WIDTH * SCREEN_HEIGHT) * 5; ++i)
 	{
 		const double c_real = fmod((rand() / 10000000.0), 4.0) - 2;
@@ -95,24 +96,78 @@ void	budha_calc(t_data *data)
 			fractal_coord_to_img_coord(data->comp, real, im, &x, &y);
 			if (x < 0 || y < 0 || x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT)
 				continue;
-			arr[x][y]++;
+			arr[x][y][0]++;
 		}
 	}
 
-	int max = 0;
+	data->max_iteration = 500;
+	for (int i = 0; i < (SCREEN_WIDTH * SCREEN_HEIGHT) * 5; ++i)
+	{
+		const double c_real = fmod((rand() / 10000000.0), 4.0) - 2;
+		const double c_im = fmod((rand() / 10000000.0), 4.0) - 2;
+
+		if (is_in_mandelbrot(c_real, c_im, data->max_iteration))
+			continue;
+
+		double	real = 0;
+		double	im = 0;
+		for (int j = 0; j < data->max_iteration; ++j)
+		{
+			double tmp_im = im;
+			im = 2 * real * im + c_im;
+			real = real * real - tmp_im * tmp_im + c_real;
+
+			int x;
+			int y;
+			fractal_coord_to_img_coord(data->comp, real, im, &x, &y);
+			if (x < 0 || y < 0 || x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT)
+				continue;
+			arr[x][y][1]++;
+		}
+	}
+
+	data->max_iteration = 50;
+	for (int i = 0; i < (SCREEN_WIDTH * SCREEN_HEIGHT) * 5; ++i)
+	{
+		const double c_real = fmod((rand() / 10000000.0), 4.0) - 2;
+		const double c_im = fmod((rand() / 10000000.0), 4.0) - 2;
+
+		if (is_in_mandelbrot(c_real, c_im, data->max_iteration))
+			continue;
+
+		double	real = 0;
+		double	im = 0;
+		for (int j = 0; j < data->max_iteration; ++j)
+		{
+			double tmp_im = im;
+			im = 2 * real * im + c_im;
+			real = real * real - tmp_im * tmp_im + c_real;
+
+			int x;
+			int y;
+			fractal_coord_to_img_coord(data->comp, real, im, &x, &y);
+			if (x < 0 || y < 0 || x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT)
+				continue;
+			arr[x][y][2]++;
+		}
+	}
+
+	int max[3] = {0};
 	for (int i = 0; i < SCREEN_WIDTH; ++i) {
 		for (int j = 0; j < SCREEN_HEIGHT; ++j) {
-			if (arr[i][j] > max)
-				max = arr[i][j];
-
+			if (arr[i][j][0] > max[0])
+				max[0] = arr[i][j][0];
+			if (arr[i][j][1] > max[1])
+				max[1] = arr[i][j][1];
+			if (arr[i][j][2] > max[2])
+				max[2] = arr[i][j][2];
 		}
 	}
 
 	for (int i = 0; i < SCREEN_WIDTH; ++i) {
 		for (int j = 0; j < SCREEN_HEIGHT; ++j) {
-			double factor = ((double)arr[i][j] / max) ;
-			factor = fmax(factor - 0.08, 0) * 0xFF;
-			draw_pixel(data->img, i, j, create_rgb(factor, factor, factor));
+			double factor[3] =  {(double)arr[i][j][0] / max[0], (double)arr[i][j][1] / max[1], (double)arr[i][j][2] / max[2]};
+			draw_pixel(data->img, i, j, create_rgb(factor[0] * 0xFF, factor[1] * 0xFF, factor[2] * 0xFF));
 		}
 	}
 
